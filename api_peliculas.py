545be278d2d9bf3e744104af1d4c523677a0e7ee
ingjfnf <161249718+ingjfnf@users.noreply.github.com>
2024-05-24 23:38:03 -0500

    Add files via upload
    primer

---
+++ api_peliculas.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask_restx import Api, Resource, fields
+from despliegue_peliculas import predict_genres
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(
+    app,
+    version='1.0',
+    title='API PREDICCIÓN DE GÉNEROS DE PELÍCULAS',
+    description='Esta es una API que utiliza un modelo de clasificación para predecir los géneros de películas. Puede probar cuantas veces quiera y con cualquier ID dentro del rango del conjunto de prueba.'
+)
+
+ns = api.namespace('predict',
+                   description='Predicción de géneros de películas')
+
+parser = api.parser()
+parser.add_argument(
+    'ID',
+    type=int,
+    required=True,
+    help='Introduzca el número del ID del conjunto de TEST que desea predecir con nuestro modelo',
+    location='args'
+)
+
+resource_fields = api.model('Resource', {
+    'Géneros Más Probables': fields.List(fields.String),
+    'Probabilidades': fields.Raw
+})
+
+@ns.route('/')
+class MovieGenresApi(Resource):
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        indice = args['ID']
+        
+        # Cargamos el conjunto de datos de prueba
+        df_test = pd.read_csv('https://github.com/albahnsen/MIAD_ML_and_NLP/raw/main/datasets/dataTesting.zip', encoding='UTF-8', index_col=0)  
+
+        try:
+            top_genres, pred_probas = predict_genres(df_test, indice)
+            return {
+                "Géneros Más Probables": top_genres,
+                "Probabilidades": pred_probas.to_dict()
+            }, 200
+        except ValueError as e:
+            return {"error": str(e)}, 404
+        except Exception as e:
+            api.abort(500, f"Error inesperado: {str(e)}")
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
---
+++ despliegue_peliculas.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import pandas as pd
+import joblib
+import sys
+import os
+
+def predict_genres(df_test, fila):
+    # Cargamos el modelo de predicción de géneros y el transformador
+    model_data = joblib.load(os.path.join(os.path.dirname(__file__), 'model_data.pkl'))
+    clf = model_data['model']
+    preprocessor = model_data['preprocessor']
+    all_genres = model_data['all_genres']
+    
+    # Verificar si el índice existe en el DataFrame
+    if fila not in df_test.index:
+        raise ValueError(f'El índice {fila} no existe en el conjunto de testing.')
+    
+    # Seleccionamos la fila que queremos predecir
+    dato = df_test.loc[[fila]]
+    
+    # Aplicamos la transformación al dato de entrada usando el preprocesador
+    transformacion = preprocessor.transform(dato)
+    
+    # Hacemos la predicción
+    y_pred_proba = clf.predict_proba(transformacion)
+    
+    # Crear un DataFrame con las probabilidades
+    prediccion = pd.DataFrame(y_pred_proba, columns=all_genres)
+    
+    # Determinar los tres géneros más probables
+    top_genres = sorted(
+        [(genre, round(prob, 3)) for genre, prob in prediccion.iloc[0].items()],
+        key=lambda x: x[1],
+        reverse=True
+    )[:3]
+
+    return top_genres, prediccion.T
+
+if __name__ == "__main__":
+
+    df_test = pd.read_csv('https://github.com/albahnsen/MIAD_ML_and_NLP/raw/main/datasets/dataTesting.zip', encoding='UTF-8', index_col=0)
+
+    if len(sys.argv) == 1:
+        print('Por favor ingrese un número de ID para evaluar en el conjunto de TEST')
+    else:
+        fila = int(sys.argv[1])
+        try:
+            top_genres, pred_probas = predict_genres(df_test, fila)
+            print(f'Los 3 géneros predichos con más alta probabilidad para la película del ID: {fila} del conjunto de prueba son: {top_genres}')
+            print("------------------------------------------------------------------------------------------")
+            print("------------------------------------------------------------------------------------------")
+            print(f'Datos de probabilidad por género para la película del ID {fila} del conjunto de prueba:\n\n{pred_probas}')
+        except ValueError as e:
+            print(e)
